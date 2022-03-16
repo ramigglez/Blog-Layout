9e5c0afebdda470be852e660e81b0ee8f27dc684
Ramiro G <ramigglez@gmail.com>
2022-03-16 13:57:07 -0600

    estructura html

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once './Strings/Strings.php';
 require_once './html/html.php';
+
 //Blog Layout
 page(
     //head content (array || string || null)
@@ -25,6 +26,201 @@ page(
                 'inline' => []
             ],              //end array of attributes
             'Blog Name'  //tag content
+        ),
+        tag(
+            'div',
+            [
+                'keyval' => [
+                    'class' => 'row'
+                ],
+                'inline' => []
+            ],
+            #row content
+            [
+                tag(
+                    'section',
+                    [
+                        'keyval' => [
+                            'class' => 'leftcolumn'
+                        ],
+                        'inline' => []
+                    ],
+                    #leftcolumn content
+                    tag(
+                        'div',
+                        [
+                            'keyval' => [
+                                'class' => 'card'
+                            ],
+                            'inline' => []
+                        ],
+                        #card content
+                        [
+                            tag(
+                                'h2',
+                                null,
+                                'TITLE HEADING'
+                            ),
+                            tag(
+                                'h5',
+                                null,
+                                'Title decription, Mar 16, 2022'
+                            ),
+                            tag(
+                                'div',
+                                [
+                                    'keyval' => [
+                                        'class' => 'fakeimg',
+                                        'style' => 'height:200px;'
+                                    ],
+                                    'inline' => []
+                                ],
+                                'Image'
+                            ),
+                            tag(
+                                'p',
+                                null,
+                                'Some Text ...'
+                            )
+                        ]
+                    )
+                ),
+                tag(
+                    'section',
+                    [
+                        'keyval' => [
+                            'class' => 'rightcolumn',
+                        ],
+                        'inline' => []
+                    ],
+                    #rightcolumn content
+                    [
+                        tag(
+                            'div',
+                            [
+                                'keyval' => [
+                                    'class' => 'card'
+                                ],
+                                'inline' => []
+                            ],
+                            #card content
+                            [
+                                tag(
+                                    'h2',
+                                    null,
+                                    'About Me'
+                                ),
+                                tag(
+                                    'div',
+                                    [
+                                        'keyval' => [
+                                            'class' => 'fakeimg',
+                                            'style' => 'height:100px;'
+                                        ],
+                                        'inline' => []
+                                    ],
+                                    'Image'
+                                ),
+                                tag(
+                                    'p',
+                                    null,
+                                    'Some Text About me...'
+                                )
+                            ]
+                        ),
+                        tag(
+                            'div',
+                            [
+                                'keyval' => [
+                                    'class' => 'card'
+                                ],
+                                'inline' => []
+                            ],
+                            #card content
+                            [
+                                tag(
+                                    'h3',
+                                    null,
+                                    'Popular Post'
+                                ),
+                                tag(
+                                    'div',
+                                    [
+                                        'keyval' => [
+                                            'class' => 'fakeimg'
+                                        ],
+                                        'inline' => []
+                                    ],
+                                    'Image'
+                                ),
+                                tag('br'),
+                                tag(
+                                    'div',
+                                    [
+                                        'keyval' => [
+                                            'class' => 'fakeimg'
+                                        ],
+                                        'inline' => []
+                                    ],
+                                    'Image'
+                                ),
+                                tag('br'),
+                                tag(
+                                    'div',
+                                    [
+                                        'keyval' => [
+                                            'class' => 'fakeimg'
+                                        ],
+                                        'inline' => []
+                                    ],
+                                    'Image'
+                                )
+                            ]
+                        ),
+                        tag(
+                            'div',
+                            [
+                                'keyval' => [
+                                    'class' => 'card'
+                                ],
+                                'inline' => []
+                            ],
+                            #card content
+                            [
+                                tag(
+                                    'h3',
+                                    null,
+                                    'Follow Me'
+                                ),
+                                tag(
+                                    'p',
+                                    null,
+                                    'Some Text ...'
+                                )
+                            ]
+                        )
+                    ]
+                )
+            ]
+        ),
+        tag(
+            'footer',
+            [
+                'keyval' => [
+                    'class' => 'footer'
+                ],
+                'inline' => []
+            ],
+            //footer content
+            tag(
+                'h2',
+                [
+                    'keyval' => [],
+                    'inline' => []
+                ],
+                //h2 content
+                '&copy; www.ramigglez.com'
+            )
         )
     ],
     //body attributes (array)
